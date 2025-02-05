18dd2b23e984b26ebbd93b06d023a0e98ba169fe
Lucas Herculano <138306297+Cyber-Herculano@users.noreply.github.com>
2025-02-05 15:49:57 -0300

    alteração

--- Testes.js
+++ Testes.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static('public')); // aqui o express aceita arquivos estáticos como CSS, bootstrap, e arquivos JS, tbm imgages
 
 app.get('/', (req, res)=>{
     res.render('Testes');
